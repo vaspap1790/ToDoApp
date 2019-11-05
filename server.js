4c86a8d428a30350f94355d004575e731e15376e
vaspap1790 <vaspap1790@gmail.com>
2019-11-05 10:54:19 +0200

    CRUD first elementary implementation

--- server.js
+++ server.js
@@ -6,15 +6,20 @@ let mongodb = require("mongodb");
 let app = express();
 let db;
 
+app.use(express.static('public'));
+
 let connectionString = "mongodb+srv://vaspap:babanila@cluster0-sb1fv.mongodb.net/ToDoApp?retryWrites=true&w=majority";
 mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
-    db = client.db();
-    app.listen(3000);
+  db = client.db();
+  app.listen(3000);
 });
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
+
+  db.collection('items').find().toArray(function (err, items) {
 
     res.send(`
     
@@ -40,43 +45,45 @@ app.get("/", function (req, res) {
     </div>
     
     <ul class="list-group pb-5">
+      ${items.map(function (item) {
+      return `      
       <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">Fake example item #1</span>
+        <span class="item-text">${item.text}</span>
         <div>
-          <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-          <button class="delete-me btn btn-danger btn-sm">Delete</button>
+          <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+          <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
         </div>
-      </li>
-      <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">Fake example item #2</span>
-        <div>
-          <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-          <button class="delete-me btn btn-danger btn-sm">Delete</button>
-        </div>
-      </li>
-      <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">Fake example item #3</span>
-        <div>
-          <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-          <button class="delete-me btn btn-danger btn-sm">Delete</button>
-        </div>
-      </li>
+      </li>`
+    }).join('')}
     </ul>
     
   </div>
-  
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+  <script src="/browser.js"></script>
 </body>
 </html>
     `);
-
+  });
 });
 
 app.post("/create-item", function (req, res) {
 
-    db.collection("items").insertOne({ text: req.body.item }, function () {
+  db.collection("items").insertOne({ text: req.body.item }, function () {
+    res.redirect('/');
 
-        res.send("Thank you");
-
-    });
+  });
 });
 
+app.post('/update-item', function (req, res) {
+
+  db.collection('items').findOneAndUpdate({ _id: new mongodb.ObjectId(req.body.id) }, { $set: { text: req.body.text } }, function () {
+    res.send("Success");
+  });
+});
+
+app.post('/delete-item', function (req, res) {
+
+  db.collection('items').deleteOne({ _id: new mongodb.ObjectId(req.body.id) }, function () {
+    res.send("Success");
+  });
+});
