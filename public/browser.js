8a14150497188c05e731b1bca9e6ad9e0c96ca4c
vaspap1790 <vaspap1790@gmail.com>
2019-11-08 12:43:07 +0200

    completed version

--- public/browser.js
+++ public/browser.js
@@ -1,4 +1,40 @@
 
+function itemTemplate(item) {
+    return `      
+    <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+      <span class="item-text">${item.text}</span>
+      <div>
+        <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+        <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+      </div>
+    </li>`;
+};
+
+//Initial Page Load Render
+let ourHTML = items.map(function (item) {
+    return itemTemplate(item)
+}).join('');
+
+document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML);
+
+//Create Feature
+const createField = document.getElementById("create-field");
+
+document.getElementById("create-form").addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    axios.post('create-item', { text: createField.value })
+        .then(function (response) {
+            document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data));
+            createField.value = '';
+            createField.focus();
+        })
+        .catch(function () {
+            console.error("Please try again later");
+        });
+});
+
 document.addEventListener("click", function (e) {
 
     //Delete feature
@@ -10,7 +46,6 @@ document.addEventListener("click", function (e) {
                 })
                 .catch(function () {
                     console.error("Please try again later");
-
                 });
         }
     }
--- server.js
+++ server.js
@@ -1,21 +1,43 @@
 
 let express = require("express");
 let mongodb = require("mongodb");
+let sanitizeHTML = require('sanitize-html');
 
 
 let app = express();
 let db;
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+};
 
 app.use(express.static('public'));
 
 let connectionString = "mongodb+srv://vaspap:babanila@cluster0-sb1fv.mongodb.net/ToDoApp?retryWrites=true&w=majority";
 mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
   db = client.db();
-  app.listen(3000);
+  app.listen(port);
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+function passwordProtected(req, res, next) {
+  res.set('WWW-Authenticate', 'Basic realm="Simple To Do App"');
+  console.log(req.headers.authorization);
+
+  if (req.headers.authorization == "Basic bGVhcm46amF2YXNjcmlwdA==") {
+    // learn , javascript
+    next();
+  }
+  else {
+    res.status(401).send("Authentication required");
+  }
+
+};
+
+app.use(passwordProtected);
 
 app.get("/", function (req, res) {
 
@@ -36,28 +58,23 @@ app.get("/", function (req, res) {
     <h1 class="display-4 text-center py-1">To-Do App</h1>
     
     <div class="jumbotron p-3 shadow-sm">
-      <form action="/create-item" method="POST">
+      <form id="create-form" action="/create-item" method="POST">
         <div class="d-flex align-items-center">
-          <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+          <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
           <button class="btn btn-primary">Add New Item</button>
         </div>
       </form>
     </div>
     
-    <ul class="list-group pb-5">
-      ${items.map(function (item) {
-      return `      
-      <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">${item.text}</span>
-        <div>
-          <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-          <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
-        </div>
-      </li>`
-    }).join('')}
+    <ul id="item-list" class="list-group pb-5">
     </ul>
     
   </div>
+
+  <script>
+    let items = ${JSON.stringify(items)}
+  </script>
+
   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
   <script src="/browser.js"></script>
 </body>
@@ -68,15 +85,17 @@ app.get("/", function (req, res) {
 
 app.post("/create-item", function (req, res) {
 
-  db.collection("items").insertOne({ text: req.body.item }, function () {
-    res.redirect('/');
+  const safeText = sanitizeHTML(req.body.text, { allowedTags: [], allowedAttributes: [] });
+  db.collection("items").insertOne({ text: safeText }, function (err, info) {
+    res.json(info.ops[0]);
 
   });
 });
 
 app.post('/update-item', function (req, res) {
 
-  db.collection('items').findOneAndUpdate({ _id: new mongodb.ObjectId(req.body.id) }, { $set: { text: req.body.text } }, function () {
+  const safeText = sanitizeHTML(req.body.text, { allowedTags: [], allowedAttributes: [] });
+  db.collection('items').findOneAndUpdate({ _id: new mongodb.ObjectId(req.body.id) }, { $set: { text: safeText } }, function () {
     res.send("Success");
   });
 });
@@ -87,3 +106,4 @@ app.post('/delete-item', function (req, res) {
     res.send("Success");
   });
 });
+
