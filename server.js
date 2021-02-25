b52a4438081a217f3550395f315e6b24b887ed2f
Vasileios Papadimitrakopoulos <vaspap1790@gmail.com>
2021-02-25 19:48:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (port == null || port == "") {
 
 app.use(express.static('public'));
 
-let connectionString = "mongodb+srv://vaspap:babanila@cluster0-sb1fv.mongodb.net/ToDoApp?retryWrites=true&w=majority";
+let connectionString = process.env.CONNECTIONSTRING;
 mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
   db = client.db();
   app.listen(port);
